ac1a5535415756291772a5ccf0244d09be35034b
Greg Palen <codingvirtual@gmail.com>
2015-05-20 11:17:35 -0500

    add comments to tournament.py and tournament.sql

--- tournament.py
+++ tournament.py
@@ -5,6 +5,38 @@
 
 import psycopg2
 
+"""
+    This application relies on a PostgreSQL database named "tournament."
+    The set up of this database is reflected in tournament.sql which can be
+    imported into PostgreSQL using \i tournament.sql.
+
+    The database consists of two tables:
+        players: a database of registered players
+            id: a unique id number for the player (primary key)
+            name: the player's full name as a single text element
+        matches: a database of the winner and loser of each match
+            winner: the id of the winning player (used as a foreign key to players)
+            loser: the id of the losing player (again, foreign key to players)
+
+    There are also several Views that make table qeuries easier:
+        match_count: provides a concise table of how many matches each player
+            has been in. Used by the standings view further down.
+                id: the id of the player
+                name: name of the player
+                matches_played: count of number of matches the player has been in
+        win_count: provides a concise table of how many wins each player has had. Used
+            by the standings view further down.
+                id: the id of the player
+                name: name of the player
+                win_count: number of wins for this player
+        standings: a concise table of current standings sorted in order from
+            most wins to least wins. It queries the other two views to compose
+            its results and extract wins and matches played for each player.
+                id: the id of the player
+                name: name of the player
+                win_count: number of wins for this player
+                matches_played: total number of matches the player has been in
+"""
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -15,6 +47,7 @@ def deleteMatches():
     """Remove all the match records from the database."""
     db = connect()
     c = db.cursor()
+    """ the following command removes all rows from table 'matches' """
     c.execute("DELETE from matches;")
     db.commit()
     db.close()
@@ -23,6 +56,7 @@ def deletePlayers():
     """Remove all the player records from the database."""
     db = connect()
     c = db.cursor()
+    """ the following command removes all rows from table 'players' """
     c.execute("DELETE from players;")
     db.commit()
     db.close()
@@ -31,10 +65,14 @@ def countPlayers():
     """Returns the number of players currently registered."""
     db = connect()
     c = db.cursor()
+    """ This query counts the number of rows in the players table, which
+    represents the total number of registered players """
+
     c.execute("SELECT count(*) from players;")
-    player_count = c.fetchone()
+    player_count = c.fetchone()   """ should only be one record """
     db.close()
-    return player_count[0]
+     """ the count will be the only element in this list, so return it """
+    return player_count[0]  
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
@@ -48,6 +86,11 @@ def registerPlayer(name):
 
     db = connect()
     c = db.cursor()
+    """ need only insert the name. The ID field is configured as 'serial'
+    so the database will automatically assign the next number in the sequence.
+    Since it is a primary key, it is guaranteed to also be unique. Note that
+    there can be multiple occurrences of a player with the same name, but each
+    will have a unique ID in the database """
     c.execute("INSERT INTO players (name) values (%s);",(name,))
     db.commit()
     db.close()
@@ -67,7 +110,11 @@ def playerStandings():
     """
     db = connect()
     c = db.cursor()
+    """ Using the standings view to easily extract current standings """
     c.execute("SELECT * FROM standings")
+    """ put the query results into a list and then return that list. Note
+    that the standings table will include players who have not yet played
+    at all (zero matches) as well as players who have never won """
     result = c.fetchall()
     db.close()
     return result
@@ -81,6 +128,7 @@ def reportMatch(winner, loser):
     """
     db = connect()
     c = db.cursor()
+    """ add a new record to the matches table reflecting the result """
     c.execute("INSERT INTO matches (winner, loser) values (%s, %s);",(winner, loser))
     db.commit()
     db.close()
@@ -103,10 +151,27 @@ def swissPairings():
     """
     db = connect()
     c = db.cursor()
+
+    """ standings are in order from most wins to most losses. The basic approach
+    of the Swiss Pairing is to take the first two players in the standings and
+    match them up, then the next two, etc. This query gets all standings records
+    and assumes there will always be an even number of players. """
     c.execute("SELECT id, name FROM standings")
+
     result = c.fetchall()
+
+    """ initialize a list to contain the pairing tuples """
     pairings=[]
+
+    """ iterate over the standings. The "current" record in the list (represented
+    by x) is used to retrieve the id and name of the first player and x+1 is
+    used to get the opponent. X is then incremented by two to get the next pair """
     for x in range(0,len(result) - 1,2):
+
+        """ create a tuple consisting of the first player id and name concatenated
+        with the 2nd player id and name. Append this to the pairings list """
         pairings.append(result[x] + result[x+1])
     db.close()
+
+    """ return the pairings list of tuples """
     return pairings
--- tournament.sql
+++ tournament.sql
@@ -6,17 +6,30 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- create the database
 create database tournament;
+
+-- connect to the database
 \c tournament;
+
+-- create the players and matches tables
 create table players (id serial primary key, name text not null);
 create table matches (winner integer not null, loser integer not null);
+
+-- create a match_count view to tally the number of matches each player
+-- has been in
 create view match_count as select players.id as ID, players.name as Name, 
 	count(matches.winner) as Matches_Played from players 
 	left join matches on matches.winner = players.id or matches.loser = players.id 
 	group by players.id;
+
+-- create a win_count view to tally the number of wins each player has
 create view win_count as select players.id as ID, players.name as Name, 
 	count(matches.winner) as win_count from players 
 	left join matches on matches.winner = players.id 
 	group by players.id;
+
+-- create a standings view that is built by extracting results from both the
+-- match_count and win_count views. Sort the results by most wins to least wins.
 create view standings as select match_count.id, match_count.name, win_count, matches_played 
 	from match_count, win_count where match_count.id = win_count.id order by win_count desc;
